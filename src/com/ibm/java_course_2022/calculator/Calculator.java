3020b57261b50b9be2dc511255247d2adeb1fcc7
User <java.user.tester@gmail.com>
2022-08-13 15:48:27 +0300

    calculator

--- src/com/ibm/java_course_2022/calculator/Calculator.java
+++ src/com/ibm/java_course_2022/calculator/Calculator.java
@@ -1,4 +1,43 @@
 package com.ibm.java_course_2022.calculator;
 
 public class Calculator {
+    /**
+     *
+     * @param operand1
+     * @param operand2
+     * @return operand1 + operand2
+     */
+    public int doSum(int operand1, int operand2){
+        return operand1 + operand2;
+    }
+
+    /**
+     *
+     * @param operand1
+     * @param operand2
+     * @return
+     */
+    public int doMinus(int operand1, int operand2){
+        return operand1 - operand2;
+    }
+
+    /**
+     *
+     * @param operand1
+     * @param operand2
+     * @return operand1/operand2
+     */
+    public int doDivide(int operand1, int operand2){
+        return operand1/operand2;
+    }
+
+    /**
+     *
+     * @param operand1
+     * @param operand2
+     * @return operand1*operand2
+     */
+    public int doMultiply(int operand1, int operand2){
+        return operand1*operand2;
+    }
 }
---
+++ src/com/ibm/java_course_2022/calculator/CalculatorTools.java
@@ -0,0 +1,62 @@
+package com.ibm.java_course_2022.calculator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CalculatorTools {
+    Calculator calculator;
+    //constructor DI
+    public CalculatorTools(Calculator calculator){
+        this.calculator = calculator;
+    }
+    public void runCalculator(){
+        Scanner sc = new Scanner(System.in);
+        doLogic(sc, calculator);
+    }
+
+    private void doLogic(@NotNull Scanner sc, Calculator calculator){
+        while(true) {
+            System.out.println("Choose math operation[type 1 or 2,...] or type [9 for exit]");
+            System.out.println("1.Multiply" +"\n" +"2.Divide" +
+                    "\n"+"3.Sum"+"\n"+ "4.Minus");
+            try {
+                int choice = sc.nextInt();//choice entered by user
+                if (choice == 9) {
+                    System.out.println("Program was finished!");
+                    return;
+                }
+                System.out.println("Enter a first operand!");
+                int operand1 = sc.nextInt();
+                System.out.println("Enter a second operand!");
+                int operand2 = sc.nextInt();
+
+            long result  = 0L;
+            switch (choice) {
+                case 1: {
+                    result = calculator.doMultiply(operand1, operand2);
+                    break;
+                }
+                case 2: {
+                    break;
+                }
+                case 3: {
+                    result = calculator.doSum(operand1, operand2);
+                    break;
+                }
+                case 4:
+                    break;
+
+                default: {
+                    System.out.println("Undefined operation");
+                }
+            }//end switch
+             System.out.printf("%s %d %n", "result of operation is:", result);
+            }catch(InputMismatchException ime){
+
+            }
+        }//end while
+    }
+
+}

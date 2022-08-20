951ef32637811e62b3950f37e8f176282f5bdcc9
User <java.user.tester@gmail.com>
2022-08-20 13:44:59 +0300

    new branch

--- src/com/ibm/java_course_2022/calculator/Calculator.java
+++ src/com/ibm/java_course_2022/calculator/Calculator.java
@@ -10,6 +10,16 @@ public class Calculator {
     public int doSum(int operand1, int operand2){
         return operand1 + operand2;
     }
+    //OOP - overloading (static polymorphism)
+    public Number doSum (Number operand1, Number operand2){
+       if(operand1 instanceof Long && operand2 instanceof Long){
+           return operand1.longValue() + operand2.longValue();
+       }else if(operand1 instanceof Double && operand2 instanceof Double){
+           return operand1.doubleValue() + operand2.doubleValue();
+       }
+           throw new RuntimeException("NaN");
+
+    }
 
     /**
      *
